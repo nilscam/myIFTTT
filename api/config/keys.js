beaf4567a3ed9bf03a651c6a9741e9c7c5a12e7a
cyril chaillan <mistralaix@MBP-cc.local>
2019-02-11 17:54:39 +0100

    add twitter et exemples trigger/reaction

--- api/config/keys.js
+++ api/config/keys.js
@@ -7,5 +7,11 @@ module.exports = {
     nasa: {
 		api_key: "4UTvkS80md4NTgLKJLFTRo9g5XMnFCpqXi7BPJQr"
     },
+    twitter: {
+		consumer_key: 'QArew40ucYgJXhW5nR3oahSKP',
+		consumer_secret: 'V9gRG60aIiyKwBiorNSF6mNJ3oJ2gN9Ac0wgdvI7Ta5fV0AmsI',
+		access_token_key: '501852948-U590JzV9q2IKTCMthytiimVRYfvNxGM88gYw6LAx',
+		access_token_secret: 'kxG6oGjoWE010wgor7mOZEa2zqtkr5Kjc0JB0rrWYXM4X'
+	},
     jwtSecret: "werkfdzxcvbnm"
 }
