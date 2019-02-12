eb34913d15b5fcce4b3f052d0808c99a478dcb8e
Unknown <nilscambreleng@gmail.com>
2019-02-12 13:08:48 +0100

    cros origin

--- api/app.js
+++ api/app.js
@@ -14,6 +14,12 @@ module.exports = {
         app.use(bodyParser.urlencoded({ extended: false }))
         app.use(bodyParser.json())
 
+        app.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+        });
+
         // connect to mongodb
         mongoose.connect(keys.mongodb.dbURL, () => {
             console.log('Connected to mongodb');
--- backend/app.js
+++ backend/app.js
@@ -16,11 +16,6 @@ module.exports = {
     launch_client: function(port) {
         const app = express();
 
-        app.use(function(req, res, next) {
-          res.header("Access-Control-Allow-Origin", "*");
-          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-          next();
-        });
         app.use(express.static(__dirname + '/../frontend/vue/dist/'));
 
         app.use(cookieSession({
