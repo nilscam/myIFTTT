a1115980e0c50ef8896370dbf9dbeda547ba5272
cyril chaillan <mistralaix@MBP-cc.local>
2019-02-16 18:14:08 +0100

    Refactor infosApplet

---
+++ api/infosApplet.js
@@ -0,0 +1,82 @@
+var infosApplet = {
+    checkNewTweet : {
+        title: "New tweet by you",
+        description: "This trigger fires every time you post a new tweet.",
+        params: {}
+    },
+    checkNewMention : {
+        title: "New mention of you",
+        description: "This trigger fires every time you are @mentioned in a tweet.",
+        params: {}
+    },
+    checkNewTweetHashtag : {
+        title: "New tweet by you with hashtag",
+        description: "This trigger fires every time you post a new tweet with specific hashtag.",
+        params: {
+            hashtag: "String"
+        }
+    },
+    checkNewFollower : {
+        title: "New follower",
+        description: "This trigger fires every time a new user starts following you.",
+        params: {}
+    },
+    checkNewLike : {
+        title: "New liked tweet by you",
+        description: "This trigger fires every time you like a tweet.",
+        params: {}
+    },
+    sendTweet : {
+        name: "sendTweet",
+        title: "Post a tweet",
+        description: "This action will post a new tweet on your account.",
+        params: {
+            tweet: "String"
+        }
+    },
+    addEveryDay : {
+        title: "Every day at",
+        description: "This trigger fires every single day at a specific time set by you.",
+        params: {
+            hour: "Hour",
+            minute: "Minute"
+        }
+    },
+    addEveryHour : {
+        title: "Every hour at",
+        description: "This trigger fires every hour at a specific time set by you.",
+        params: {
+            minute: "Minute"
+        }
+    },
+    addEveryDayOfTheWeek : {
+        title: "Every day of the week at",
+        description: "This trigger fires only on specific days of the week at the time you provide.",
+        params: {
+            day: "Day",
+            hour: "Hour",
+            minute: "Minute"
+        }
+    },
+    addEveryMonth : {
+        title: "Every month on the",
+        description: "This trigger fires every month on the day and time you specify.",
+        params: {
+            Day: "Number",
+            hour: "Hour",
+            minute: "Minute"
+        }
+    },
+    addEveryYear : {
+        title: "Every year on",
+        description: "This trigger fires every year on the date and time you specify.",
+        params: {
+            month: "Number",
+            day: "Number",
+            hour: "Hour",
+            minute: "Minute"
+        }
+    },
+}
+
+module.exports = infosApplet;
--- api/routes/services.js
+++ api/routes/services.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/user-model').User;
 const checkAuth = require('../middleware/check-auth');
+const infosApplet = require('../infosApplet');
 
 router.get('/', checkAuth, (req, res) => {
     res.status(200).send({
@@ -13,45 +14,29 @@ router.get('/', checkAuth, (req, res) => {
                 triggers: [
                     {
                         name: "checkNewTweet",
-                        title: "New tweet by you",
-                        description: "This trigger fires every time you post a new tweet.",
-                        params: {}
+                        infos: infosApplet['checkNewTweet'],
                     },
                     {
                         name: "checkNewMention",
-                        title: "New mention of you",
-                        description: "This trigger fires every time you are @mentioned in a tweet.",
-                        params: {}
+                        infos: infosApplet['checkNewMention'],
                     },
                     {
                         name: "checkNewTweetHashtag",
-                        title: "New tweet by you with hashtag",
-                        description: "This trigger fires every time you post a new tweet with specific hashtag.",
-                        params: {
-                            hashtag: "String"
-                        }
+                        infos: infosApplet['checkNewTweetHashtag'],
                     },
                     {
                         name: "checkNewFollower",
-                        title: "New follower",
-                        description: "This trigger fires every time a new user starts following you.",
-                        params: {}
+                        infos: infosApplet['checkNewFollower'],
                     },
                     {
                         name: "checkNewLike",
-                        title: "New liked tweet by you",
-                        description: "This trigger fires every time you like a tweet.",
-                        params: {}
+                        infos: infosApplet['checkNewLike'],
                     },
                 ],
                 reactions: [
                     {
                         name: "sendTweet",
-                        title: "Post a tweet",
-                        description: "This action will post a new tweet on your account.",
-                        params: {
-                            tweet: "String"
-                        }
+                        infos: infosApplet['sendTweet'],
                     }
                 ]
             },
@@ -63,51 +48,23 @@ router.get('/', checkAuth, (req, res) => {
                 triggers: [
                     {
                         name: "addEveryDay",
-                        title: "Every day at",
-                        description: "This trigger fires every single day at a specific time set by you.",
-                        params: {
-                            hour: "Hour",
-                            minute: "Minute"
-                        }
+                        infos: infosApplet['addEveryDay'],
                     },
                     {
                         name: "addEveryHour",
-                        title: "Every hour at",
-                        description: "This trigger fires every hour at a specific time set by you.",
-                        params: {
-                            minute: "Minute"
-                        }
+                        infos: infosApplet['addEveryHour'],
                     },
                     {
                         name: "addEveryDayOfTheWeek",
-                        title: "Every day of the week at",
-                        description: "This trigger fires only on specific days of the week at the time you provide.",
-                        params: {
-                            day: "Day",
-                            hour: "Hour",
-                            minute: "Minute"
-                        }
+                        infos: infosApplet['addEveryDayOfTheWeek'],
                     },
                     {
                         name: "addEveryMonth",
-                        title: "Every month on the",
-                        description: "This trigger fires every month on the day and time you specify.",
-                        params: {
-                            Day: "Number",
-                            hour: "Hour",
-                            minute: "Minute"
-                        }
+                        infos: infosApplet['addEveryMonth'],
                     },
                     {
                         name: "addEveryYear",
-                        title: "Every year on",
-                        description: "This trigger fires every year on the date and time you specify.",
-                        params: {
-                            month: "Number",
-                            day: "Number",
-                            hour: "Hour",
-                            minute: "Minute"
-                        }
+                        infos: infosApplet['addEveryYear'],
                     },
                 ],
                 reactions: {}
