9af22733c730d1530fac584a829ae48a142f3af4
boris <boris.roussel@epitech.eu>
2019-03-05 11:54:58 +0100

    insta test connection / mailler mail

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,6 +5,7 @@ const keys = require('../config/keys');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const request = require('request-promise')
 const checkAuth = require('../middleware/check-auth');
 const googleAuth = require('./auth/google-auth')
 
@@ -133,6 +134,29 @@ router.post('/twitter', checkAuth, (req, res) => {
             res.status(500).send({error: "User not found"});
         }
 	});
+});
+
+router.post('/instagram', checkAuth, (req, res) => {
+    User.findOne({_id: req.userData.userId}).then((currentUser) => {
+        if (currentUser) {
+            currentUser._services._instagram._token = req.body.token;
+            request({
+                url: 'https://api.instagram.com/v1/users/self/?access_token=' + req.body.token,
+            }, function (error, response, body) {
+                if (error || response.statusCode !== 200) {
+                    res.status(500).send({error: "Bad credential"});
+                } else {
+                    currentUser._services._instagram._id = body.data.id;
+                    currentUser._services._instagram._username = body.data.username;
+                    currentUser._services._instagram._photo = body.data.profile_picture;
+                    currentUser.save();
+                    res.status(200).send({error: null});
+                };
+            })
+        } else {
+            res.status(500).send({error: "User not found"});
+        }
+    });
 });
 
 module.exports = router;
