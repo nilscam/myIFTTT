0cfc429823310d435c782cebc5943b2f6063fd8b
cyril chaillan <mistralaix@MBP-cc.local>
2019-03-10 19:20:21 +0100

    Clean useless files

--- api/routes/reactions.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const instagramRoutes = require('./reactions/instagramReaction');
-const mailerRoutes = require('./reactions/mailerReaction');
-
-router.use('/instagram', instagramRoutes);
-router.use('/mailer', mailerRoutes);
-
-module.exports = router;
